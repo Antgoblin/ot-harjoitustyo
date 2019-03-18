c6631ea474b8af916e34ee605e3418760f17f343
antonpaa <anton.paavola@helsinki.fi>
2019-03-18 18:54:23 +0200

    Unicafe testit tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinLuominenToimii() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaRahaaToimii() {
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaToimiiKunSaldoaTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosOttaaLiikaa() {
+        kortti.otaRahaa(12);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPaluttaaTruejosRahatRiittaa() {
+        assertEquals(true, kortti.otaRahaa(2));
+    }
+    
+    @Test
+    public void otaRahaaPaluttaaFalsejosRahatEiRiittaa() {
+        assertEquals(false, kortti.otaRahaa(100));
+    }
 }

33ebb95d15b020a1ee33cfc881d1e148bdd7fa28
antonpaa <anton.paavola@helsinki.fi>
2019-03-23 00:43:23 +0200

    layout improvements

--- DungeonCrawler/src/main/java/dungeoncrawler/DungeonCrawlerSovellus.java
+++ DungeonCrawler/src/main/java/dungeoncrawler/DungeonCrawlerSovellus.java
@@ -8,6 +8,7 @@ package dungeoncrawler;
 import dungeoncrawler.Tile;
 import java.util.List;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 //import javafx.scene.canvas.Canvas;
 //import javafx.scene.canvas.GraphicsContext;
@@ -20,6 +21,8 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.canvas.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
@@ -28,11 +31,11 @@ import javafx.scene.paint.Color;
  * @author jy
  */
 public class DungeonCrawlerSovellus extends Application {
-    
+
     public static void main(String[] args) {
         launch();
     }
-    
+
     public static int WIDTH = 950;
     public static int HEIGHT = 750;
     public static int tileSize = 50;
@@ -40,112 +43,132 @@ public class DungeonCrawlerSovellus extends Application {
     private Map map;
     int mapSize = 100;
     private Player player;
-    
+
     public void init() {
         map = new Map(mapSize);
-        
-        player = new Player(5,4,1);
-        
+
+        player = new Player(5, 4, 1);
+
         for (int y = 0; y < map.getSize(); y++) {
             for (int x = 0; x < map.getSize(); x++) {
                 Tile tile = map.getTile(x, y);
 
-                if(x == 1  || y == 1) {
+                if (x == 1 || y == 1 || x == 17 || y == 17) {
                     tile.setWall();
-                }                
+                }
             }
         }
     }
-    
-    
-    
+
     @Override
     public void start(Stage Dungeon) {
-        
-        //BorderPane layout = new BorderPane();
-        //layout.setPrefSize(WIDTH*50, HEIGHT*50);
-        
-        Pane screen = new Pane();
+
+        GridPane screen = new GridPane();
         screen.setPrefSize(WIDTH, HEIGHT);
-        
-        //Pane side = new Pane();
-        //side.setPrefSize(WIDTH*10, HEIGHT*55);
-        
+        screen.setHgap(0);
+        screen.setVgap(0);
+//        screen.setPadding(new Insets(5, 5, 5, 5));
+        screen.toBack();
+
         Canvas canvas = new Canvas(mapSize * tileSize, mapSize * tileSize);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         
-        Pane camera = new Pane();
-        camera.setPrefSize(WIDTH, HEIGHT);
-        int cameraMaxX = WIDTH/(2*tileSize) +1;
-        int cameraMaxY = HEIGHT/(2*tileSize) +1;
-        
-        
+        TextArea textArea = new TextArea();
+        textArea.setMinSize(200, 200);
+        textArea.setEditable(false);
+        textArea.setFocusTraversable(false);
+        textArea.setMouseTransparent(true);
+
+        TextArea statscreen = new TextArea();
+        statscreen.setMinSize(300, HEIGHT);
+        statscreen.setEditable(false);
+        statscreen.setFocusTraversable(false);
+        statscreen.setMouseTransparent(true);
+        Text hp = new Text("hp");
+        statscreen.appendText("aaa");
+
+        int cameraMaxX = WIDTH / (2 * tileSize) + 1;
+        int cameraMaxY = HEIGHT / (2 * tileSize) + 1;
+
         drawTiles(gc);
         drawGrid(gc);
         drawPlayer(gc);
         
-        screen.getChildren().add(canvas);
-        Scene game = new Scene(screen);
-        
+        screen.add(canvas, 1, 1, 1, 1); //1,1
+        screen.add(textArea, 0, 0, 2, 1); //0,0
+        screen.add(statscreen, 0, 1, 1, 1);
+        Scene game = new Scene(screen, WIDTH + 300, HEIGHT + 200);
+
         game.setOnKeyPressed(event -> {
-            if(event.getCode() == KeyCode.UP) {
-                player.moveUp();
-                if (player.getY() >= cameraMaxY - 1) {
-                    screen.setTranslateY(screen.getTranslateY() + tileSize);
+            if (event.getCode() == KeyCode.UP) {
+                if (map.getTile(player.getX(), player.getY() - 1).isWall() == false) {
+                    player.moveUp();
+                    if (player.getY() >= cameraMaxY - 1) {
+                        canvas.setTranslateY(canvas.getTranslateY() + tileSize);
+                    }
                 }
-                
-            } else if(event.getCode() == KeyCode.DOWN) {
-                player.moveDown();
-                if (player.getY() >= cameraMaxY) {
-                    screen.setTranslateY(screen.getTranslateY() - tileSize);
+
+            } else if (event.getCode() == KeyCode.DOWN) {
+                if (map.getTile(player.getX(), player.getY() + 1).isWall() == false) {
+                    player.moveDown();
+                    if (player.getY() >= cameraMaxY) {
+                        canvas.setTranslateY(canvas.getTranslateY() - tileSize);
+                    }
+
                 }
-                
-            } else if(event.getCode() == KeyCode.RIGHT) {
-                player.moveRight();
-                if (player.getX() >= cameraMaxX) {
-                    screen.setTranslateX(screen.getTranslateX() - tileSize);
+
+            } else if (event.getCode() == KeyCode.RIGHT) {
+                if (map.getTile(player.getX() + 1, player.getY()).isWall() == false) {
+                    player.moveRight();
+                    if (player.getX() >= cameraMaxX) {
+                        canvas.setTranslateX(canvas.getTranslateX() - tileSize);
+                    }
+
                 }
-                
-            } else if(event.getCode() == KeyCode.LEFT) {
-                player.moveLeft();
-                if (player.getX() >= cameraMaxX - 1 ) {
-                    screen.setTranslateX(screen.getTranslateX() + tileSize);
+
+            } else if (event.getCode() == KeyCode.LEFT) {
+                if (map.getTile(player.getX() - 1, player.getY()).isWall() == false) {
+                    player.moveLeft();
+                    if (player.getX() >= cameraMaxX - 1) {
+                        canvas.setTranslateX(canvas.getTranslateX() + tileSize);
+                    }
+
                 }
             }
             drawTiles(gc);
             drawGrid(gc);
             drawPlayer(gc);
-            
+
         });
-        
+
         Dungeon.setTitle("DungeonCrawler");
         Dungeon.setScene(game);
-        Dungeon.setResizable(true);
+        Dungeon.setResizable(false);
         Dungeon.show();
-        
+
     }
-    
+
     private void drawGrid(GraphicsContext gc) {
         gc.setStroke(Color.BLACK);
-        for (int i = 0; i < map.getSize() +1 ; i++) {            
+        for (int i = 0; i < map.getSize() + 1; i++) {
             gc.strokeLine(0, i * tileSize, map.getSize() * tileSize, i * tileSize);
         }
-        for (int j = 0; j < map.getSize() +1 ; j++) {            
+        for (int j = 0; j < map.getSize() + 1; j++) {
             gc.strokeLine(j * tileSize, 0, j * tileSize, map.getSize() * tileSize);
         }
-        
+
     }
-    
+
     private void drawPlayer(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
-        gc.fillRect(player.getX() * tileSize + (tileSize - playerSize)/2, player.getY() * tileSize + (tileSize - playerSize)/2 , playerSize, playerSize);
+        gc.fillRect(player.getX() * tileSize + (tileSize - playerSize) / 2, player.getY() * tileSize + (tileSize - playerSize) / 2, playerSize, playerSize);
     }
-    
+
     private void drawTiles(GraphicsContext gc) {
-        for(int y = 0; y < map.getSize(); y++ ) {
-            for(int x = 0; x < map.getSize(); x++ ) {
+        for (int y = 0; y < map.getSize(); y++) {
+            for (int x = 0; x < map.getSize(); x++) {
                 if (map.getTile(x, y).isWall()) {
-                    gc.setFill(Color.BLACK);                    
+                    gc.setFill(Color.BLACK);
                 } else {
                     gc.setFill(Color.WHITE);
                 }
@@ -153,5 +176,5 @@ public class DungeonCrawlerSovellus extends Application {
             }
         }
     }
-    
+
 }
--- DungeonCrawler/src/main/java/dungeoncrawler/Tile.java
+++ DungeonCrawler/src/main/java/dungeoncrawler/Tile.java
@@ -17,38 +17,16 @@ import javafx.scene.shape.Rectangle;
 public class Tile extends StackPane {
     
     private Boolean solid;
-    private Rectangle border;
-    private Color fillColor;
-    private Color borderColor;
         
     public Tile() {
-//        this.border = new Rectangle(50, 50);
-//        border.setFill(null);
-//        border.setStroke(Color.BLACK);
-//
-//        setAlignment(Pos.CENTER);
-//        getChildren().addAll(border);
         this.solid = false;
-//        this.fillColor = null;
-//        this.borderColor = Color.BLACK;
-//        this.setTranslateX(x);
-//        this.setTranslateY(y);
+
     }
-    
-//    public void render() {
-//        border.setFill(fillColor);
-//        border.setStroke(borderColor);
-//        
-//        setAlignment(Pos.CENTER);
-//        getChildren().addAll(border);
-//        
-//    }
-//    
+  
     public void setWall() {
-//        this.fillColor = Color.BLACK;
         this.solid = true;
     }
-//    
+    
     public Boolean isWall() {
         return this.solid;
     }
